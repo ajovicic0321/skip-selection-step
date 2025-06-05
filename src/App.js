b147c011f2402d2aae5de844b0d189ec44ee78ab
ajovicic0321 <aleks.jovicic0321@gmail.com>
2025-06-05 11:02:47 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SkipSelection from './components/SkipSelection';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App; 
